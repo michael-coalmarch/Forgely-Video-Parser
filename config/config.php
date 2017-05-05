ae43c04f2e4b69c3a61069a76ba5c1d2eed3ded8
michael-coalmarch <mquattrochi@coalmarch.com>
2017-05-05 10:09:58 -0400

    renamed adapter paths in config.php

--- config/config.php
+++ config/config.php
@@ -13,20 +13,20 @@ return array(
                 '#(https?://vimeo.com)/([0-9]+)#i',
                 '#(https?://vimeo.com)/channels/staffpicks/([0-9]+)#i',
             ),
-            'factory' => '\\RicardoFiorani\\Adapter\\Vimeo\\Factory\\VimeoServiceAdapterFactory',
+            'factory' => '\\Coalmarch\\Adapter\\Vimeo\\Factory\\VimeoServiceAdapterFactory',
         ),
         'Youtube' => array(
             'patterns' => array(
                 '#(?:<\>]+href=\")?(?:http://)?((?:[a-zA-Z]{1,4}\.)?youtube.com/(?:watch)?\?v=(.{11}?))[^"]*(?:\"[^\<\>]*>)?([^\<\>]*)(?:)?#',
                 '%(?:youtube\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i',
             ),
-            'factory' => '\\RicardoFiorani\\Adapter\\Youtube\\Factory\\YoutubeServiceAdapterFactory',
+            'factory' => '\\Coalmarch\\Adapter\\Youtube\\Factory\\YoutubeServiceAdapterFactory',
         ),
         'Dailymotion' => array(
             'patterns' => array(
                 '#https?://www.dailymotion.com/video/([A-Za-z0-9]+)#s',
             ),
-            'factory' => '\\RicardoFiorani\\Adapter\\Dailymotion\\Factory\\DailymotionServiceAdapterFactory',
+            'factory' => '\\Coalmarch\\Adapter\\Dailymotion\\Factory\\DailymotionServiceAdapterFactory',
         ),
         'Facebook' => array(
             'patterns' => array(
@@ -35,11 +35,11 @@ return array(
                 '~^https?://www\.facebook\.com/.*?/videos/(\d+)/?$~m',
 
             ),
-            'factory' => '\\RicardoFiorani\\Adapter\\Facebook\\Factory\\FacebookServiceAdapterFactory',
+            'factory' => '\\Coalmarch\\Adapter\\Facebook\\Factory\\FacebookServiceAdapterFactory',
         ),
     ),
     'renderer' => array(
         'name' => 'DefaultRenderer',
-        'factory' => '\\RicardoFiorani\\Renderer\\Factory\\DefaultRendererFactory',
+        'factory' => '\\Coalmarch\\Renderer\\Factory\\DefaultRendererFactory',
     )
 );
